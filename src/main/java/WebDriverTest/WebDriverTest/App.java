61dc721b10f428e1dfaaf5e89a0ffb5b904671de
anuroopharshini <pandiri.harshini@gmail.com>
2021-11-12 20:17:10 -0800

    first commit

---
+++ src/main/java/WebDriverTest/WebDriverTest/App.java
@@ -0,0 +1,18 @@
+package WebDriverTest.WebDriverTest;
+import org.junit.Test;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+
+
+{
+	
+	@Test
+    public void test1()
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/WebDriverTest/WebDriverTest/AppTest.java
@@ -0,0 +1,17 @@
+package WebDriverTest.WebDriverTest;
+
+import org.junit.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+@Test
+public void test1()
+{
+    System.out.println( "Hello World!" );
+}
+}
