75202fed093a1efe566893877f7ff95eab467a57
anuroopharshini <pandiri.harshini@gmail.com>
2021-11-15 17:06:25 -0800

    stashchange

---
+++ src/main/java/WebDriverTest/WebDriverTest/TESTSTASH.java
@@ -0,0 +1,12 @@
+package WebDriverTest.WebDriverTest;
+
+public class TESTSTASH {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=20;
+
+	}
+
+}
